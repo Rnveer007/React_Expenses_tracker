1e65dfb6daf052258bd864ecea5ceaef8dc643c4
Rnveer007 <rvs523474@gmail.com>
2025-02-12 14:36:51 +0530

    updated

--- ExpensesTracker/src/App.jsx
+++ ExpensesTracker/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1>Expenses Tracker</h1>
+          <div>
+            <input type="text" placeholder='Enter Your Amount' />
+            <button>Set Budget</button>
+          </div>
+
+          <div>
+            <h1>Total :- </h1>
+            <h1>Spent :- </h1>
+            <h1>Remain :- </h1>
+          </div>
+        </div>
+
+        <div>
+          <h1>Expenses</h1>
+          <div>
+            <input type="text" placeholder='Enter Expense' />
+            <input type="text" placeholder='Enter Amount' />
+            <button>Add Expense</button>
+          </div>
+
+          <table>
+            <th>S.N.</th>
+            <th>Info</th>
+            <th>Amount</th>
+            <tr>
+              <td>1</td>
+              <td>Food</td>
+              <td>200</td>
+            </tr>
+          </table>
+        </div>
+
+
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
